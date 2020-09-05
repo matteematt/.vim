b4bf19025e84255c5b87bc95bc6f0a783e78dea2
Matt Walker <mwa75@bskyb.com>
2020-09-05 17:50:35 +0100

    Fixed the formatting of the errorlines for the scala linter

--- bin/scalalinter.sh
+++ bin/scalalinter.sh
@@ -10,8 +10,8 @@ else
 	# Run the linter with the filename being $1 and format for quickfix window using awk
 	scalastyle -c "$config_file" "$1" | awk '{
 		if (match($0,/(?:error|warning)\sfile=(.+)\smessage=(.+)\s+line=(.+)\scolumn=(.+)/,m))
-			print sprintf("%s:%s:%s %s: %s", m[2], m[4], m[5], m[1], m[3])
+			print sprintf("%s:%s:%s: %s - %s", m[2], m[4], m[5], m[1], m[3])
 		else if (match($0,/(?:error|warning)\sfile=(.+)\smessage=(.+)\s+line=(.+)/,m))
-			print sprintf("%s:%s:1 %s: %s", m[2], m[4], m[1], m[3])
+			print sprintf("%s:%s:0: %s - %s", m[2], m[4], m[1], m[3])
 		}'
 fi
